06aef087032e46be380bda7d06553f70d1bc3327
Johnny Egeland <johnnyege@gmail.com>
2022-09-25 12:39:23 +0200

    Added second example

---
+++ src/Ex02_LocalVariableInference.java
@@ -0,0 +1,29 @@
+/**
+ * Demonstrates local variable inference
+ */
+public class Ex02_LocalVariableInference {
+
+    /**
+     * Really useful class
+     */
+    static class TheLongestMostRidiculousClassNameICouldComeUpWithForDemonstratingWhereTypeInferenceShines {
+        void sayHello() {
+            System.out.println("Well, hello there...");
+        }
+    }
+
+    /**
+     * Demonstrates local type inference
+     * @param args Because I get yelled at for using var
+     */
+    public static void main(String[] args) {
+        System.out.println("Example 02: Type inference demonstrated");
+
+        // Create an instance
+        TheLongestMostRidiculousClassNameICouldComeUpWithForDemonstratingWhereTypeInferenceShines instance =
+                new TheLongestMostRidiculousClassNameICouldComeUpWithForDemonstratingWhereTypeInferenceShines();
+
+        System.out.println("Saying hello...");
+        instance.sayHello();
+    }
+}
---
+++ src/solution/Ex01_InterfacePrivateAndDefault.java
@@ -0,0 +1,50 @@
+package solution;
+
+/**
+ * Example of Interface with private methods
+ */
+public class Ex01_InterfacePrivateAndDefault {
+
+    /**
+     * Interface for providers of numbers
+     */
+    interface NumberProvider {
+        double number();
+
+        default double squared() {
+            return raisedToPower(2);
+        }
+
+        default double qubed() {
+            return raisedToPower(3);
+        }
+
+        private double raisedToPower(int power) {
+            return Math.pow(number(), power);
+        }
+    }
+
+    /**
+     * Provides the best number there is
+     */
+    static class AnswerToLifeTheUniverseAndEverythingProvider implements NumberProvider {
+        @Override
+        public double number() {
+            return 42;
+        }
+    }
+
+    /**
+     * Runs the Example
+     * @param args In case we meet Pirates
+     */
+    public static void main(String[] args) {
+        System.out.println("Example 01: Default, static and private interface methods");
+
+        AnswerToLifeTheUniverseAndEverythingProvider provider = new AnswerToLifeTheUniverseAndEverythingProvider();
+        System.out.println("- The best number: " + provider.number());
+        System.out.println("- The best number squared: " + provider.squared());
+        System.out.println("- The best number qubed: " + provider.qubed());
+
+    }
+}
