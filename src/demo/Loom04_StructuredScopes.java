d04f5f462328f97281739327cbfafac7b6ed9ffd
Johnny Egeland <johnnyege@gmail.com>
2022-10-04 22:03:20 +0200

    Minor change

--- src/demo/Loom04_StructuredScopes.java
+++ src/demo/Loom04_StructuredScopes.java
@@ -42,27 +42,7 @@ public class Loom04_StructuredScopes {
         }
     }
 
-    private static void runningTask2() throws InterruptedException {
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            var start = System.currentTimeMillis();
-            var future1 = scope.<Integer>fork(() -> {
-                throw new AssertionError("oops");
-            });
-            var future2 = scope.fork(() -> {
-                Thread.sleep(1_000);
-                System.out.println("end");
-                return 2;
-            });
-            scope.join();
-            var end = System.currentTimeMillis();
-            System.out.println("elapsed " + (end - start));
-            var result = future1.resultNow() + future2.resultNow();
-            System.out.println(result);
-        }
-    }
-
     public static void main(String[] args) throws InterruptedException {
         simpleExample();
-        runningTask2();
     }
 }
