4e2f43380a886a097ba73fcfd5775b16673bc871
Johnny Egeland <johnnyege@gmail.com>
2022-10-05 20:57:51 +0200

    Fixed example

--- src/demo/Loom04_StructuredScopes.java
+++ src/demo/Loom04_StructuredScopes.java
@@ -17,32 +17,35 @@ public class Loom04_StructuredScopes {
             var start = System.currentTimeMillis();
             // :: Fork two async tasks
             var future1 = scope.fork(() -> {
-                /*
-                if (true) {
-                    throw new AssertionError("Boom!");
-                }
-                //*/
+                //if (true) throw new AssertionError("Boom!");
                 Thread.sleep(1_000);
                 System.out.println("task1 end");
                 return 1;
-
             });
             var future2 = scope.fork(() -> {
                 Thread.sleep(1_000);
                 System.out.println("task2 end");
                 return 2;
             });
+            var future3 = scope.fork(() -> {
+                Thread.sleep(1_000);
+                System.out.println("task3 end");
+                return 3;
+            });
+
             // :: Wait for tasks to complete, and the use results
             scope.join();
             var end = System.currentTimeMillis();
             System.out.println("elapsed " + (end - start));
-            var result = future1.resultNow() + future2.resultNow();
-            //var result = future1.resultNow();
+
+            // Sum the results
+            var result = future1.resultNow() + future2.resultNow() + future3.resultNow();
+            //var result = future2.resultNow() + future3.resultNow();
             System.out.println(result);
         }
     }
 
-    // Include ShutdownOnSuccess and ShutdownOnFailure examples as well
+    // TODO! Show ShutdownOnSuccess and ShutdownOnFailure
     // https://download.java.net/java/early_access/loom/docs/api/jdk.incubator.concurrent/jdk/incubator/concurrent/StructuredTaskScope.html
 
     public static void main(String[] args) throws InterruptedException {
